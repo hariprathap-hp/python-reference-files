f6fed4bca05b10fa77d42b58b683fcd309b7dce6
Hari Prathap <hariprathap89@gmail.com>
2018-04-19 17:55:14 +0530

    v0.3 with issue with unicode characters fixed
    v0.3 with issue with unicode/special characters fixed. IMDB data for all the year can now be fetched

--- imdb.py
+++ imdb.py
@@ -1,43 +1,47 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 11 14:19:29 2018
-
 @author: Hari Prathap
 """
 
-import scrapy, json, re, openpyxl
-my_file = open("C:\\Users\\Aricent\\tutorial\\imdb1.json",'w+')
+import scrapy, json, re, os
+js_file = " "
 oscar_2018 = {"Oscars Categories":"Oscar Winners"}
+year = ""
 
 class ParseIMDB(scrapy.Spider):
     name = 'imdb'
-    
-    
-    def start_requests(self):
+
+    def start_requests(self):   
         urls = [
-                'https://www.imdb.com/oscars/nominations/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=4395ef7d-6c05-4720-b03d-8d31fc0af5dd&pf_rd_r=412QRQE1Z96VSCF5WKGW&pf_rd_s=top-1&pf_rd_t=60601&pf_rd_i=oscars&ref_=fea_acd_nav_i3'
-                ]
-        
+                'https://www.imdb.com/event/ev0000003/2016/1/?ref_=ev_eh',
+                'https://www.imdb.com/event/ev0000003/2017/1/?ref_=ev_eh'
+                ]        
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_imdb)
             
     def parse_imdb(self,response):
+        my_file = open("C:\\Users\\Aricent\\tutorial\\javascript.json",'wb+')
         js_output = response.xpath('//span[@class="ab_widget"]//script[@type="text/javascript"]').extract()
-        for line in js_output[0]:
-            my_file.write(line)    
+        my_file.write(js_output[0].encode("utf-8"))
         self.extract_movie_info()
             
     def extract_movie_info(self):
         print("In function")
-        json_file = open("C:\\Users\\Aricent\\tutorial\\imdb1.json")
-        my_json_out = open("C:\\Users\\Aricent\\tutorial\\imdb_out.json","w+")
+        json_file = open("C:\\Users\\Aricent\\tutorial\\javascript.json")
+        my_json_out = open("C:\\Users\\Aricent\\tutorial\\final_json.json","w+")
         my_lines = json_file.readlines()
-        my_rex = re.compile(r'(?<=\'center-8-react\',)(.*)(?=]\);)')
+        #print(my_lines)
+        my_rex = re.compile(r'(?<=\'center-(\d)-react\',)(.*)(?=]\);)') 
         for line in my_lines:
-            if "center-8-react" in line:
+            if "nomineesWidgetModel" in line:      
                 my_line = my_rex.search(line)
+                if my_rex.search(line):
+                    print("my_rex")
+                else:
+                    print("fuck off")
                 my_json_out.write(my_line.group())
-        my_file = open("C:\\Users\\Aricent\\tutorial\\imdb_out.json")
+        my_file = open("C:\\Users\\Aricent\\tutorial\\final_json.json")
         json_data = json.load(my_file)
         length = len(json_data["nomineesWidgetModel"]["eventEditionSummary"]["awards"][0]["categories"])
         print(length)
@@ -48,3 +52,7 @@ class ParseIMDB(scrapy.Spider):
                 winners = json_data["nomineesWidgetModel"]["eventEditionSummary"]["awards"][0]["categories"][i]["nominations"][0]["primaryNominees"][0]["name"]
                 oscar_2018[categories] = winners
         print(oscar_2018)
+        my_file.close()
+        my_json_out.close()
+        os.remove("C:\\Users\\Aricent\\tutorial\\javascript.json")
+        os.remove("C:\\Users\\Aricent\\tutorial\\final_json.json")
